8f9ab7f4dcc8bddae25df76a58abaa173e6743ac
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-04-05 13:06:05 +0900

    スクロールでナビゲーションバーを隠したりするアニメーションを実装

--- EveryoneZoo/TimeLineViewController.swift
+++ EveryoneZoo/TimeLineViewController.swift
@@ -1,16 +1,14 @@
 //
-//  PictureViewController.swift
+//  TimeLineViewController.swift
 //  EveryoneZoo
 //
-//  Created by junpei ono on 2017/03/17.
+//  Created by junpei ono on 2017/03/21.
 //  Copyright © 2017年 junpei ono. All rights reserved.
 //
 
 import UIKit
 
-
-class PictureViewController: UIViewController,UIScrollViewDelegate {
-    
+class TimeLineViewController: UIViewController {
     
     var statusHeight:CGFloat!
     var navBarHeight:CGFloat!
@@ -21,13 +19,11 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
     
     var scrollViewHeight:CGFloat!
     
-    
-    var myNavBar:UINavigationBar!
-    
     var pictureScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         //画面横サイズを取得
         viewWidth = self.view.frame.width
@@ -38,27 +34,25 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
         
         //ナビゲーションバーのサイズの取得
         navBarHeight = 40
-
+        
         //タブバーの高さの取得
         tabBarHeight = 40
         
         //スクロルビュー
-        scrollViewHeight = viewHeight-(statusHeight+navBarHeight)
+        scrollViewHeight = viewHeight-(statusHeight+navBarHeight+tabBarHeight)
         
         setView()
         
         
-    
+        
         // ScrollViewを生成.
         pictureScrollView = UIScrollView()
         
-        self.pictureScrollView.delegate = self
-        
         // ScrollViewの大きさを設定する.
         pictureScrollView.frame = CGRect(x: 0, y: statusHeight+navBarHeight, width: viewWidth, height: scrollViewHeight)
-        /*
+        
         // UIImageに画像を設定する.
-        let examplePic = UIImage(named: "example_pictures")!
+        let examplePic = UIImage(named: "example_timeline")!
         
         // UIImageViewを生成する.
         let exampleImageView = UIImageView()
@@ -74,19 +68,23 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
         
         // ScrollViewにmyImageViewを追加する.
         pictureScrollView.addSubview(exampleImageView)
-        */
+        
         // Scrollの高さを計算しておく.
-        let scroll_height = viewHeight*2
+        let scroll_height = examplePic.size.height*(viewWidth/examplePic.size.width)
         
         // ScrollViewにcontentSizeを設定する.
         pictureScrollView.contentSize = CGSize(width:viewWidth, height:scroll_height)
-
+        
+        
         pictureScrollView.backgroundColor = UIColor.red
         // ViewにScrollViewをAddする.
         self.view.addSubview(pictureScrollView)
- 
+
+        
+        // Do any additional setup after loading the view.
     }
-    
+
+
     //Viewへの配置
     func setView() {
         
@@ -94,7 +92,7 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
         self.view.backgroundColor = UtilityLibrary.getZooThemeColor()
         
         //UINavigationBarを作成
-        myNavBar = UINavigationBar()
+        let myNavBar = UINavigationBar()
         
         //UINavigationBarの位置とサイズを指定
         myNavBar.frame = CGRect(x: 0, y: statusHeight, width: viewWidth, height: navBarHeight)
@@ -110,7 +108,7 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
         
         //ナビゲーションアイテムを作成
         let myNavItems = UINavigationItem()
-        myNavItems.title = "ひろば"
+        myNavItems.title = "タイムライン"
         
         //バーの左側に設置するボタンの作成
         let leftNavBtn =  UIBarButtonItem(barButtonSystemItem:  .search, target: self, action: #selector(leftBarBtnClicked(sender:)))
@@ -120,59 +118,6 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
         myNavBar.pushItem(myNavItems, animated: true)
         self.view.addSubview(myNavBar)
         
-        print("上")
-
-  
-    }
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>){
-        
-
-        if (velocity.y > 0) {
-            // 上下のバーを隠す
-            print("上")
-            
-            hidesBarsWithScrollView(hidden: true, hiddenTop: true, hiddenBottom: true)
-            
-            // 上下のバーを表示する
-        } else {
-            // 上下のバーを表示する
-            print("した")
-            hidesBarsWithScrollView(hidden: false, hiddenTop: true, hiddenBottom: true)
-        }
-    }
-    
-    
-    func hidesBarsWithScrollView( hidden:Bool, hiddenTop:Bool, hiddenBottom:Bool) {
-        
-        let timing = UICubicTimingParameters(animationCurve: .easeInOut)
-        let animator = UIViewPropertyAnimator(duration: 0.5, timingParameters: timing)
-        
-        
-        if hidden {
-            animator.addAnimations {
-                // animation
-                self.myNavBar.frame = CGRect(x: 0, y: -(self.statusHeight+self.navBarHeight), width: self.viewWidth, height: self.navBarHeight)
-                self.pictureScrollView.frame = CGRect(x: 0, y: 0, width: self.viewWidth, height: self.viewHeight)
-
-            }
-        }else{
-            animator.addAnimations {
-                // animation
-                self.myNavBar.frame = CGRect(x: 0, y: self.statusHeight, width: self.viewWidth, height: self.navBarHeight)
-                self.pictureScrollView.frame = CGRect(x: 0, y: self.statusHeight+self.navBarHeight, width: self.viewWidth, height: self.scrollViewHeight)
-
-            }
-        }
-        
-        
-        
-        animator.startAnimation()
-        
-        /*
-        myNavBar.animateWithDuration(5, animations: { () -> Void in
-            myNavBar.backgroundColor = UIColor.greenColor()
-        })*/
     }
     
     
@@ -180,8 +125,5 @@ class PictureViewController: UIViewController,UIScrollViewDelegate {
     internal func leftBarBtnClicked(sender: UIButton){
         print("leftBarBtnClicked")
     }
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
+
 }
