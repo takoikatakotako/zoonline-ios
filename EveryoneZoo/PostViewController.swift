ee4b405278089de0c605976a44892bf7de20ec5c
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-05-10 00:57:34 +0900

    ナビゲーションバーのカスタムバーを作成した。

--- EveryoneZoo/PostViewController.swift
+++ EveryoneZoo/PostViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class PostViewController: UIViewController {
+    
+    //width, height
+    private var statusHeight:CGFloat!
+    private var navBarHeight:CGFloat!
+    private var segmentViewHeight:CGFloat!
+    private var tabBarHeight:CGFloat!
+    private var viewWidth:CGFloat!
+    private var viewHeight:CGFloat!
+    private var tableViewHeight:CGFloat!
+    
+    //view parts
+    private var pictureTableView: UITableView!
+    @IBOutlet weak var serchNavBtn: UIBarButtonItem!
+    
 
     //テーブルビューインスタンス
     private var myTableView: UITableView!
@@ -18,13 +32,82 @@ class PostViewController: UIViewController {
         super.viewDidLoad()
         
         //Viewの大きさを取得
-        let viewWidth = self.view.frame.size.width
-        let viewHeight = self.view.frame.size.height
+        viewWidth = self.view.frame.size.width
+        viewHeight = self.view.frame.size.height
         
-        let samplePostView:UIImageView = UIImageView()
-        samplePostView.image = UIImage(named: "post_sample")
-        samplePostView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight)
-        self.view.addSubview(samplePostView)
+        
+        //各部品の高さを取得
+        statusHeight = UIApplication.shared.statusBarFrame.height
+        navBarHeight =  self.navigationController?.navigationBar.frame.height
+        segmentViewHeight = self.navigationController?.navigationBar.frame.height
+        tabBarHeight = UITabBar.appearance().frame.size.height
+        //tableViewHeight = viewHeight-(statusHeight+navBarHeight+segmentViewHeight+tabBarHeight+tabBarHeight)
+        
+        
+        
+        //ステータスバー部分の背景
+        let statusBackColor = UIView()
+        statusBackColor.frame = CGRect(x: 0, y: 0, width: viewWidth, height: 60)
+        statusBackColor.backgroundColor = UIColor.mainAppColor()
+        self.view.addSubview(statusBackColor)
+        
+
+        setView()
+    }
+    
+    func setView() {
+        
+        //ナビゲーションバーの作成
+        let myNavBar:ZooNavigationBar = ZooNavigationBar()
+        myNavBar.frame = CGRect(x: 0, y: PARTS_HEIGHT_STATUS_BAR, width: viewWidth, height: PARTS_HEIGHT_NAVIGATION_BAR)
+        
+        
+        
+        myNavBar.myView.backgroundColor = UIColor.blue
+        
+        
+        //ナビゲーションボタンの色を変更する
+        //UINavigationBar.appearance().tintColor = UIColor.white
+        
+        //ナビゲーションアイテムを作成
+        let myNavItems = UINavigationItem()
+        //myNavItems.titleLabe.text = "バーのタイトル"
+        
+        //バーの左側に設置するボタンの作成
+        let leftNavBtn =  UIBarButtonItem(barButtonSystemItem:  .add, target: self, action: #selector(leftBarBtnClicked(sender:)))
+        myNavItems.leftBarButtonItem = leftNavBtn
+        
+        //バーの右側に設置するボタンの作成
+        let rightNavBtn = UIBarButtonItem()
+        
+        //ボタンにする画像を選択する
+        let rightNavBtnImg:UIImage = UIImage(named:"tab_kabi")!
+        rightNavBtn.image = rightNavBtnImg
+        
+        //ボタンが押され時のアクションを設定する
+        rightNavBtn.action = #selector(rightBarBtnClicked(sender:))
+        myNavItems.rightBarButtonItem = rightNavBtn;
+        
+        //作成したNavItemをNavBarに追加する
+        myNavBar.pushItem(myNavItems, animated: true)
+        self.view.addSubview(myNavBar)
+        
+        
+        //UIScrollView
+        let scrollView:UIScrollView = UIScrollView()
+        scrollView.frame =  CGRect(x: 0, y: PARTS_HEIGHT_STATUS_BAR, width: viewWidth, height: 300)
+        scrollView.backgroundColor = UIColor.gray
+        //self.view.addSubview(scrollView)
+    }
+    
+    //左側のボタンが押されたら呼ばれる
+    internal func leftBarBtnClicked(sender: UIButton){
+        print("leftBarBtnClicked")
+    }
+    
+    //右側のボタンが押されたら呼ばれる
+    internal func rightBarBtnClicked(sender: UIButton){
+        print("rightBarBtnClicked")
     }
 
 }
---
+++ EveryoneZoo/ViewPartsSize.swift
@@ -0,0 +1,15 @@
+//
+//  ViewPartsSize.swift
+//  EveryoneZoo
+//
+//  Created by junpei ono on 2017/05/09.
+//  Copyright © 2017年 junpei ono. All rights reserved.
+//
+
+import UIKit
+
+let PARTS_HEIGHT_STATUS_BAR:CGFloat! = 22
+let PARTS_HEIGHT_NAVIGATION_BAR:CGFloat! = 44
+let PARTS_TABBAR_HEIGHT:CGFloat! = 50
+
+
