11250500bfd7f0b1076cbf5c7a7eaa5a2adf3a01
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-06-22 16:40:08 +0900

    ログイン画面の実装

--- EveryoneZoo/LoginViewController.swift
+++ EveryoneZoo/LoginViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController ,UITextFieldDelegate{
 
     //width,height
     private var viewWidth:CGFloat!
     private var viewHeight:CGFloat!
     private var loginViewHeight:CGFloat!
-
+    
+    //ViewParts
+    private var contentsScrollView:UIScrollView!
+    var mailTextField:UITextField! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +26,11 @@ class LoginViewController: UIViewController {
         viewWidth = self.view.frame.width
         viewHeight = self.view.frame.height
         loginViewHeight = viewHeight - (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)
+        
         self.view.backgroundColor = UIColor.white
         
         //Viewにパーツを追加
         setNavigationBar()
-        
         setView()
     }
     
@@ -36,10 +39,10 @@ class LoginViewController: UIViewController {
     func setNavigationBar() {
         
         //ステータスバー部分の覆い
-        let aadView:UIView = UIView()
-        aadView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: PARTS_HEIGHT_STATUS_BAR*2)
-        aadView.backgroundColor = UIColor.mainAppColor()
-        self.view.addSubview(aadView)
+        let statusView:UIView = UIView()
+        statusView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: PARTS_HEIGHT_STATUS_BAR*2)
+        statusView.backgroundColor = UIColor.mainAppColor()
+        self.view.addSubview(statusView)
         
         //ナビゲーションコントローラーの色の変更
         let navBar = UINavigationBar()
@@ -60,7 +63,6 @@ class LoginViewController: UIViewController {
         //バーの左側に設置するボタンの作成
         let leftNavBtn =  UIBarButtonItem(title: "閉じる", style: .plain, target: self, action:  #selector(leftBarBtnClicked(sender:)))
         navItems.leftBarButtonItem = leftNavBtn
-        
         navBar.pushItem(navItems, animated: true)
         self.view.addSubview(navBar)
     }
@@ -68,60 +70,99 @@ class LoginViewController: UIViewController {
     // MARK: ナビゲーションバー
     func setView() {
         
-        //Description
-        let descriptionLabel:UILabel = UILabel()
-        descriptionLabel.text = "投稿・タイムラインを利用する\nにはログインしてください"
-        descriptionLabel.numberOfLines = 2
-        descriptionLabel.textAlignment = NSTextAlignment.center
-        descriptionLabel.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.05, width: viewWidth*0.8, height: loginViewHeight*0.1)
-        self.view.addSubview(descriptionLabel)
+        //BaseScrollView
+        contentsScrollView = UIScrollView()
+        contentsScrollView.frame =  CGRect(x: 0, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR), width: viewWidth, height: loginViewHeight)
+        contentsScrollView.contentSize = CGSize(width:viewWidth, height:loginViewHeight*2)
+        
+        self.view.addSubview(contentsScrollView)
+        
+        //Logo
+        let logoImgView:UIImageView = UIImageView()
+        logoImgView.image = UIImage(named:"logo")
+        logoImgView.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.1, width: viewWidth*0.8, height: loginViewHeight*0.2)
+        logoImgView.contentMode = UIViewContentMode.scaleAspectFit
+        contentsScrollView.addSubview(logoImgView)
         
         //Login failed
         let loginFailed:UILabel = UILabel()
         loginFailed.text = "ログインできませんでした"
         loginFailed.textAlignment = NSTextAlignment.center
-        loginFailed.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.15, width: viewWidth*0.8, height: loginViewHeight*0.1)
+        loginFailed.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.28, width: viewWidth*0.8, height: loginViewHeight*0.1)
         loginFailed.textColor = UIColor.LogInPinkColor()
-        self.view.addSubview(loginFailed)
+        contentsScrollView.addSubview(loginFailed)
         
         //MailTest
-        let mailTextField:UITextField = UITextField()
-        mailTextField.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.25, width: viewWidth*0.8, height: loginViewHeight*0.1)
+        mailTextField = UITextField()
+        mailTextField.delegate = self
+        mailTextField.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.38, width: viewWidth*0.8, height: loginViewHeight*0.1)
         mailTextField.text = "ユーザー名またはメールアドレス"
         mailTextField.textColor = UIColor.gray
-        mailTextField.borderStyle = UITextBorderStyle.roundedRect
-        self.view.addSubview(mailTextField)
-    
+        mailTextField.borderStyle = UITextBorderStyle.none
+        mailTextField.font = UIFont.systemFont(ofSize: 16)
+        //mailTextField.textAlignment = NSTextAlignment.center
+        contentsScrollView.addSubview(mailTextField)
+        
+        //MailUnderLine
+        let mailTextFieldLine:UIView = UIView()
+        mailTextFieldLine.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.48, width: viewWidth*0.8, height: 1)
+        mailTextFieldLine.backgroundColor = UIColor.gray
+        contentsScrollView.addSubview(mailTextFieldLine)
+        
         //MailTest
         let passWordTextField:UITextField = UITextField()
-        passWordTextField.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.35, width: viewWidth*0.8, height: loginViewHeight*0.1)
+        passWordTextField.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.48, width: viewWidth*0.8, height: loginViewHeight*0.1)
         passWordTextField.text = "パスワード"
         passWordTextField.textColor = UIColor.gray
-        passWordTextField.borderStyle = UITextBorderStyle.roundedRect
-        self.view.addSubview(passWordTextField)
+        passWordTextField.borderStyle = UITextBorderStyle.none
+        contentsScrollView.addSubview(passWordTextField)
+        
+        //MailUnderLine
+        let passWordTextFieldLine:UIView = UIView()
+        passWordTextFieldLine.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.58, width: viewWidth*0.8, height: 1)
+        passWordTextFieldLine.backgroundColor = UIColor.gray
+        contentsScrollView.addSubview(passWordTextFieldLine)
         
         //LoginButton
         let loginBtn:UIButton = UIButton()
-        loginBtn.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.55, width: viewWidth*0.8, height: loginViewHeight*0.1)
+        loginBtn.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.65, width: viewWidth*0.8, height: viewWidth*0.15)
         loginBtn.backgroundColor = UIColor.gray
         loginBtn.setTitle("ログイン", for: UIControlState.normal)
-        self.view.addSubview(loginBtn)
-        
-        //ResisterButton
-        let resisterBtn:UIButton = UIButton()
-        resisterBtn.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.7, width: viewWidth*0.8, height: loginViewHeight*0.1)
-        resisterBtn.backgroundColor = UIColor.LogInPinkColor()
-        resisterBtn.setTitle("アカウント登録", for: UIControlState.normal)
-        self.view.addSubview(resisterBtn)
+        contentsScrollView.addSubview(loginBtn)
         
         //ForgetPassWordButton
         let forgetPassWordButton:UIButton = UIButton()
-        forgetPassWordButton.frame = CGRect(x: viewWidth*0.1, y: (PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR)+loginViewHeight*0.85, width: viewWidth*0.8, height: loginViewHeight*0.1)
+        forgetPassWordButton.frame = CGRect(x: viewWidth*0.1, y: loginViewHeight*0.8, width: viewWidth*0.8, height: viewWidth*0.15)
         forgetPassWordButton.backgroundColor = UIColor.white
         forgetPassWordButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
         forgetPassWordButton.setTitle("パスワードを忘れた方", for: UIControlState.normal)
-        self.view.addSubview(forgetPassWordButton)
+        contentsScrollView.addSubview(forgetPassWordButton)
+    }
+    
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing: \(textField.text!)")
+        self.contentsScrollView.setContentOffset(CGPoint(x: 0, y: 210), animated: true)
+    }
+    
+    
+    
+    
+    //UITextFieldが編集された直後に呼ばれる
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing: \(textField.text!)")
         
+    }
+    
+    //改行ボタンが押された際に呼ばれる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn \(textField.text!)")
+        
+        // 改行ボタンが押されたらKeyboardを閉じる処理.
+        mailTextField.resignFirstResponder()
+        
+        return true
     }
     
     //左側のボタンが押されたら呼ばれる
---
+++ EveryoneZoo/NewResistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewResistViewController.swift
+//  EveryoneZoo
+//
+//  Created by junpei ono on 2017/06/21.
+//  Copyright © 2017年 junpei ono. All rights reserved.
+//
+
+import UIKit
+
+class NewResistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
