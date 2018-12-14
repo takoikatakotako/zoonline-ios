5dc801a9f5e90901bbc5231a1e90095085b28c4f
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2018-12-14 12:56:21 +0900

    お気に入り画面で落ちないようにした。

--- zoonline-ios/MyFavoritePostsViewController.swift
+++ zoonline-ios/MyFavoritePostsViewController.swift
@@ -4,126 +4,74 @@ import SwiftyJSON
 import SDWebImage
 import SCLAlertView
 
-class MyFavoritePostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MyFavoritePostsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    //
+    var friendsCollectionView: UICollectionView!
+    private var frindsList: JSON = []
 
-    var userID: Int!
-
-    //width, height
-    private var viewWidth: CGFloat!
-    private var viewHeight: CGFloat!
-    private var statusBarHeight: CGFloat!
-    private var navigationBarHeight: CGFloat!
-    private var tabBarHeight: CGFloat!
-
-    private var tableViewHeight: CGFloat!
-
-    var favoriteListTableView: UITableView!
-    private var favoritePosts: JSON = []
+    var indicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Viewの大きさを取得
-        viewWidth = self.view.frame.size.width
-        viewHeight = self.view.frame.size.height
-        statusBarHeight = (self.navigationController?.navigationBar.frame.origin.y)!
-        navigationBarHeight = (self.navigationController?.navigationBar.frame.size.height)!
-        tabBarHeight = (self.tabBarController?.tabBar.frame.size.height)!
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem = backButton
 
-        tableViewHeight = viewHeight - (statusBarHeight+navigationBarHeight+tabBarHeight)
+        view.backgroundColor = .white
+        //テーブルビューの初期化
+        let collectionFrame = view.frame
 
-        self.view.backgroundColor = UIColor.white
-
-        setNavigationBar()
-
-        favoriteListTableView = UITableView()
-        favoriteListTableView.delegate = self
-        favoriteListTableView.dataSource = self
-        favoriteListTableView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: tableViewHeight)
-        favoriteListTableView.rowHeight = viewWidth*0.28
-
-        //テーブルビューの設置
-        favoriteListTableView.register(MyPagePostCell.self, forCellReuseIdentifier: NSStringFromClass(MyPagePostCell.self))
-        self.view.addSubview(favoriteListTableView)
-
-        getMyFavoritePosts()
+        // CollectionViewのレイアウトを生成.
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: view.frame.width/3, height: view.frame.width/3)
+        layout.sectionInset = UIEdgeInsets.zero
+        layout.minimumInteritemSpacing = 0.0
+        layout.minimumLineSpacing = 0.0
+        layout.headerReferenceSize = CGSize(width: 0, height: 0)
+        friendsCollectionView = UICollectionView(frame: collectionFrame, collectionViewLayout: layout)
+        friendsCollectionView.register(PostsCollectionViewCell.self, forCellWithReuseIdentifier: NSStringFromClass(PostsCollectionViewCell.self))
+        friendsCollectionView.delegate = self
+        friendsCollectionView.dataSource = self
+        friendsCollectionView.backgroundColor = UIColor.white
+        view.addSubview(friendsCollectionView)
     }
 
-    func getMyFavoritePosts() {
-
-        Alamofire.request(EveryZooAPI.getFavoritePosts(userID: userID)).responseJSON { response in
-
-            switch response.result {
-            case .success:
-
-                let json: JSON = JSON(response.result.value ?? kill)
-                print(json)
-
-                if json["is_success"].boolValue {
-
-                    self.favoritePosts = json["responce"]
-                    self.favoriteListTableView.reloadData()
-                }else {
-                    SCLAlertView().showInfo("エラー", subTitle: "お気に入りの投稿の取得に失敗しました。")
-                }
-
-            case .failure(let error):
-                print(error)
-                //テーブルの再読み込み
-            }
-        }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let width = view.frame.width
+        let height = view.frame.height
+        friendsCollectionView.frame = CGRect(x: 0, y: 0, width: width, height: height)
     }
 
-    // MARK: - Viewにパーツの設置
-    // MARK: ナビゲーションバーの設定
-    func setNavigationBar() {
+    // MARK: くるくるの生成
+    func setActivityIndicator() {
 
-        self.navigationController?.navigationBar.barTintColor = UIColor.init(named: "main")
-        self.navigationController?.navigationBar.isTranslucent = false
-
-        //ナビゲーションアイテムを作成
-        let titleLabel: NavigationBarLabel = NavigationBarLabel()
-        titleLabel.frame = CGRect(x: viewWidth*0.3, y: 0, width: viewWidth*0.4, height: navigationBarHeight)
-        titleLabel.textAlignment = NSTextAlignment.center
-        titleLabel.text = "お気に入り"
-        titleLabel.textColor = UIColor.white
-
-        self.navigationItem.titleView = titleLabel
+        // indicator.frame = CGRect(x: viewWidth*0.35, y: viewHeight*0.25, width: viewWidth*0.3, height: viewWidth*0.3)
+        indicator.clipsToBounds = true
+        // indicator.layer.cornerRadius = viewWidth*0.3*0.3
+        indicator.hidesWhenStopped = true
+        indicator.style = UIActivityIndicatorView.Style.whiteLarge
+        self.view.bringSubviewToFront(indicator)
+        indicator.color = UIColor.init(named: "main")
+        self.view.addSubview(indicator)
     }
 
     // MARK: テーブルビューのセルの数を設定する
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //テーブルビューのセルの数はmyItems配列の数とした
-        return self.favoritePosts.count
+    // Cellが選択された際に呼び出される
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //画面遷移、ユーザー情報画面へ
+        let postDetail: PostDetailViewController = PostDetailViewController()
+        self.navigationController?.pushViewController(postDetail, animated: true)
     }
 
-    // MARK: テーブルビューのセルの中身を設定する
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: MyPagePostCell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(MyPagePostCell.self))! as! MyPagePostCell
-        let dates = UtilityLibrary.parseDates(text: self.favoritePosts[indexPath.row]["created_at"].stringValue)
-        var dateText: String = dates["year"]! + "/"
-        dateText += dates["month"]! + "/"
-        dateText += dates["day"]!
-        cell.dateLabel.text = dateText
+    // Cellの総数を返す
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
 
-        cell.titleLabel.text = self.favoritePosts[indexPath.row]["title"].stringValue
-        cell.commentLabel.text = self.favoritePosts[indexPath.row]["caption"].stringValue
-
-        let imageUrlText = self.favoritePosts[indexPath.row]["image_url"].stringValue
-        if let imageUrl = URL(string: imageUrlText) {
-            cell.thumbnailImg.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "sample_loading"))
-        }
+    // Cellに値を設定する
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PostsCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: NSStringFromClass(PostsCollectionViewCell.self), for: indexPath) as! PostsCollectionViewCell
         return cell
-    }
-
-    // MARK: テーブルビューのセルが押されたら呼ばれる
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //画面遷移、投稿詳細画面へ
-        let picDetailView: PostDetailViewController = PostDetailViewController()
-        //picDetailView.postID = self.favoritePosts[indexPath.row]["id"].intValue
-
-        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        self.navigationItem.backBarButtonItem = backButton
-        self.navigationController?.pushViewController(picDetailView, animated: true)
     }
 }
--- zoonline-ios/PostCommentViewController.swift
+++ zoonline-ios/PostCommentViewController.swift
@@ -6,6 +6,10 @@ class PostCommentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
+        let rightNavBtn = UIBarButtonItem(title: "投稿", style: .plain, target: self, action: #selector(postComments(sender:)))
+        self.navigationItem.rightBarButtonItem = rightNavBtn
+
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(keyboardWillShow),
@@ -33,5 +37,8 @@ class PostCommentViewController: UIViewController {
         let keyboardWidth = width - margin * 2
         let keyboardHeight = height - margin - keyboardHeight
         commentTextView.frame = CGRect(x: margin, y: margin, width: keyboardWidth, height: keyboardHeight)
+    }
+
+    @objc internal func postComments(sender: UIButton) {
     }
 }
